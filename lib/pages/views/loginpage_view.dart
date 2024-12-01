439a8e2cb76d6e900642f10da4a59092fab92b81
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-12-02 03:50:54 +0700

    fix: login button overflow

--- lib/pages/views/loginpage_view.dart
+++ lib/pages/views/loginpage_view.dart
@@ -16,119 +16,125 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(228, 224, 225, 1),
       appBar: WavyAppbar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.1,
-          ),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Login",
-                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Login to continue using the app",
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            ),
-          ),
-          const SizedBox(height: 30),
-          CustomTextField(
-            labelText: "Email",
-            controller: controller.emailController,
-            hintText: "Enter your email",
-            obscureText: false,
-          ),
-          const SizedBox(height: 20),
-          CustomTextField(
-            labelText: "Password",
-            controller: controller.passwordController,
-            hintText: "Enter your password",
-            obscureText: true,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Obx(
-                () => Row(
-                  children: [
-                    Checkbox(
-                      value: controller.rememberMe.value,
-                      onChanged: (bool? value) {
-                        controller.rememberMe.value = value ?? false;
-                      },
-                    ),
-                    const Text("Remember Me"),
-                  ],
-                ),
-              ),
-              TextButton(
-                onPressed: () => Get.toNamed('/forgot'),
-                child: const Text(
-                  "Forgot Password?",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-            ],
-          ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: SizedBox(
-              width: double.infinity,
-              child: Obx(
-                () => ElevatedButton(
-                  onPressed: controller.isLoading.value
-                      ? null
-                      : controller.login,
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    backgroundColor: const Color.fromRGBO(91, 56, 12, 0.74),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: controller.isLoading.value
-                      ? const CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                              Colors.white),
-                        )
-                      : const Text(
-                          'Login',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold),
-                        ),
-                ),
-              ),
-            ),
-          ),
-          Row(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text("Don't have an account?"),
-              TextButton(
-                onPressed: () => Get.toNamed('/register'),
-                child: const Text(
-                  "Register",
-                  style: TextStyle(color: Colors.black),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
+              const SizedBox(height: 10),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "Login to continue using the app",
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              CustomTextField(
+                labelText: "Email",
+                controller: controller.emailController,
+                hintText: "Enter your email",
+                obscureText: false,
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                labelText: "Password",
+                controller: controller.passwordController,
+                hintText: "Enter your password",
+                obscureText: true,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Obx(
+                    () => Row(
+                      children: [
+                        Checkbox(
+                          value: controller.rememberMe.value,
+                          onChanged: (bool? value) {
+                            controller.rememberMe.value = value ?? false;
+                          },
+                        ),
+                        const Text("Remember Me"),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () => Get.toNamed('/forgot'),
+                    child: const Text(
+                      "Forgot Password?",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Obx(
+                    () => ElevatedButton(
+                      onPressed: controller.isLoading.value
+                          ? null
+                          : controller.login,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        backgroundColor: const Color.fromRGBO(91, 56, 12, 0.74),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: controller.isLoading.value
+                          ? const CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                  Colors.white),
+                            )
+                          : const Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: () => Get.toNamed('/register'),
+                    child: const Text(
+                      "Register",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
