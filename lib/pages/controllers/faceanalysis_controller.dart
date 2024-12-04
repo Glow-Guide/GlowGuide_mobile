11a5a5ebfa8237bcc09803919bb7f0fbbfa21c94
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-12-05 03:27:39 +0700

    fix: invalid double error fix

--- lib/pages/controllers/faceanalysis_controller.dart
+++ lib/pages/controllers/faceanalysis_controller.dart
@@ -53,31 +53,38 @@ class FaceAnalysisController extends GetxController {
     }
   }
 
-  Map<String, dynamic> parseFeatures(String featureString) {
-    final featureList = featureString
-        .replaceAll('[', '')
-        .replaceAll(']', '')
-        .split(RegExp(r'\s+'))
-        .where((element) => element.isNotEmpty)
-        .map((e) => double.parse(e))
-        .toList();
+ Map<String, dynamic> parseFeatures(String featureString) {
+  final featureList = featureString
+      .replaceAll('[', '')
+      .replaceAll(']', '')
+      .split(RegExp(r'\s+'))
+      .where((element) => element.isNotEmpty)
+      .map((e) {
+        try {
+          return double.parse(e);
+        } catch (error) {
+          debugPrint('Error parsing feature value: $e');
+          return 0.0; 
+        }
+      })
+      .toList();
 
-    final featureNames = [
-      'mean_L', 'var_L', 'mean_a', 'var_a', 'mean_b', 'var_b', 'mean_H', 'var_H',
-      'mean_S', 'var_S', 'mean_V', 'var_V', 'mean_Cb', 'var_Cb', 'mean_Cr', 'var_Cr',
-      'mean_RI', 'var_RI', 'LBP_bin_0', 'LBP_bin_1', 'LBP_bin_2', 'LBP_bin_3',
-      'LBP_bin_4', 'LBP_bin_5', 'LBP_bin_6', 'LBP_bin_7', 'LBP_bin_8', 'LBP_bin_9',
-      'LBP_bin_10', 'LBP_bin_11', 'LBP_bin_12', 'LBP_bin_13', 'LBP_bin_14', 'LBP_bin_15',
-      'LBP_bin_16', 'LBP_bin_17', 'LBP_bin_18', 'LBP_bin_19', 'LBP_bin_20', 'LBP_bin_21',
-      'LBP_bin_22', 'LBP_bin_23', 'LBP_bin_24', 'LBP_bin_25', 'edge_density', 'num_contours',
-      'mean_contour_area', 'std_contour_area'
-    ];
+  final featureNames = [
+    'mean_L', 'var_L', 'mean_a', 'var_a', 'mean_b', 'var_b', 'mean_H', 'var_H',
+    'mean_S', 'var_S', 'mean_V', 'var_V', 'mean_Cb', 'var_Cb', 'mean_Cr', 'var_Cr',
+    'mean_RI', 'var_RI', 'LBP_bin_0', 'LBP_bin_1', 'LBP_bin_2', 'LBP_bin_3',
+    'LBP_bin_4', 'LBP_bin_5', 'LBP_bin_6', 'LBP_bin_7', 'LBP_bin_8', 'LBP_bin_9',
+    'LBP_bin_10', 'LBP_bin_11', 'LBP_bin_12', 'LBP_bin_13', 'LBP_bin_14', 'LBP_bin_15',
+    'LBP_bin_16', 'LBP_bin_17', 'LBP_bin_18', 'LBP_bin_19', 'LBP_bin_20', 'LBP_bin_21',
+    'LBP_bin_22', 'LBP_bin_23', 'LBP_bin_24', 'LBP_bin_25', 'edge_density', 'num_contours',
+    'mean_contour_area', 'std_contour_area'
+  ];
 
-    final featureMap = <String, dynamic>{};
-    for (int i = 0; i < featureNames.length; i++) {
-      featureMap[featureNames[i]] = featureList[i];
-    }
-
-    return featureMap;
+  final featureMap = <String, dynamic>{};
+  for (int i = 0; i < featureNames.length; i++) {
+    featureMap[featureNames[i]] = featureList.length > i ? featureList[i] : 0.0;
   }
+
+  return featureMap;
+}
 }
