5f21693f34a513ca189d88346c75eb520906eb2c
firstiaa <firstiaazh@gmail.com>
2024-12-09 19:50:12 +0700

    Edit Home Page (2)

--- lib/pages/views/home_view.dart
+++ lib/pages/views/home_view.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:prototpye_glowguide/widgets/wavyappbar.dart';
 import 'package:prototpye_glowguide/widgets/custom_navbar.dart';
 import '../controllers/home_controller.dart';
 import 'contentdetails_view.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  int _current = 0;
+  final CarouselController _carouselController = CarouselController();
+
+  List<List<String>> list = [
+    [
+      'lib/assets/create_account.jpg',
+      'Step 1: Create an Account and Register',
+    ],
+    [
+      'lib/assets/complete_form.jpg',
+      'Step 2: Complete Personal Data',
+    ],
+    [
+      'lib/assets/take_picture.jpg',
+      'Step 3: Take a Face Photo with Selfie or from Gallery',
+    ],
+    [
+      'lib/assets/crop_image.jpg',
+      'Step 4: Crop the Photo on the Acne Area',
+    ],
+    [
+      'lib/assets/save_image.jpg',
+      'Step 5: Save Photo to View Prediction Results',
+    ],
+    [
+      'lib/assets/diagnosis.jpg',
+      'Step 6: Acne Diagnosis Results',
+    ],
+    [
+      'lib/assets/recommendation.jpg',
+      'Step 7: Get Skincare Ingredients Recommendations',
+    ],
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +66,122 @@ class HomePage extends StatelessWidget {
                   style: const TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 72, 35, 21),
                   ),
                   textAlign: TextAlign.start,
                 ),
+              ),
+            ),
+            // Carousel for guidance
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                children: [
+                 CarouselSlider.builder(
+                    itemCount: list.length,
+                    itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) =>
+                      Card(
+                        elevation: 4,
+                        shadowColor: Colors.black.withOpacity(0.2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                list[itemIndex][0],
+                                fit: BoxFit.contain,
+                                height: 80, 
+                                width: double.infinity,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                list[itemIndex][1],
+                                style: const TextStyle(
+                                  fontSize: 16, 
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 72, 35, 21),
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    options: CarouselOptions(
+                      height: 200, 
+                      initialPage: 0,
+                      autoPlay: true,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _current = index;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(list.length, (index) {
+                      return Container(
+                        width: 8.0,
+                        height: 8.0,
+                        margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _current == index
+                              ? Colors.brown.shade600
+                              : Colors.grey,
+                        ),
+                      );
+                    }),
+                  ),
+                ],
+              ),
+            ),
+            // Product Recommendation
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Treatment',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 72, 35, 21),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  GridView.count(
+                    crossAxisCount: 3,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      _buildProductCard('lib/assets/facewash.png', 'Facewash'),
+                      _buildProductCard('lib/assets/moisturizer.png', 'Moisturizer'),
+                      _buildProductCard('lib/assets/sunscreen.png', 'Sunscreen'),
+                      _buildProductCard('lib/assets/serum.png', 'Serum'),
+                      _buildProductCard('lib/assets/toner.png', 'Toner'),
+                      _buildProductCard('lib/assets/mask.png', 'Mask'),
+                      _buildProductCard('lib/assets/pore.png', 'Pore Strips'),
+                      _buildProductCard('lib/assets/injection.png', 'Treatment'),
+                      _buildProductCard('lib/assets/medicine.png', 'Medicine'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Glow Hack',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 72, 35, 21),
               ),
             ),
             const Center(
@@ -51,7 +204,7 @@ class HomePage extends StatelessWidget {
               child: HoverContent(
                 description:
                     'To keep your skin healthy and problem-free, it is important to understand your skin type. Regularly cleansing your face is a crucial first step, followed by diligently exfoliating to remove dead skin cells. Consuming tomatoes is also very beneficial, because it is rich in antioxidants that help maintain healthy skin. In addition, controlling water levels in the body is very important, so exercising and getting enough rest must also be considered. Finally, choosing the right product according to your skin type can be done with the help of the GlowGuide application, which helps find the right product.',
-                titleText: 'Discover the secret to smooth, acne-free skin!',
+                titleText: 'Unlock the key to flawless, acne free skin!',
                 image: AssetImage('lib/assets/smooth.jpg'),
               ),
             ),
@@ -59,12 +212,44 @@ class HomePage extends StatelessWidget {
               child: HoverContent(
                 description:
                     'Treating acne effectively requires a combination of the right treatments and healthy habits. First, make sure to wash your face twice a day with a gentle cleanser that’s right for your skin type, so dirt and oil don’t clog your pores. Use products that contain active ingredients like benzoyl peroxide or salicylic acid to help reduce inflammation and prevent breakouts.',
-                titleText: 'Effective way to deal with acne!',
+                titleText: 'Powerful acne solutions!',
                 image: AssetImage('lib/assets/product.jpg'),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildProductCard(String imagePath, String productName) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.all(8.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(imagePath),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            productName,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 14),
+          ),
+        ],
       ),
     );
   }
@@ -144,6 +329,13 @@ class _HoverContentState extends State<HoverContent>
               decoration: BoxDecoration(
                 color: const Color.fromRGBO(219, 205, 197, 0.74),
                 borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 8,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
               ),
               height: Get.width * 0.45,
               width: MediaQuery.of(context).size.width * 0.9,
@@ -155,7 +347,7 @@ class _HoverContentState extends State<HoverContent>
                     widget.titleText,
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 15, // font size for title
+                      fontSize: 15,
                     ),
                     textAlign: TextAlign.center,
                   ),
